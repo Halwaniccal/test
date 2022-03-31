0d91a98649fc3fb9a0871ca004662bc10e5ad3c9
Taha El-Helwani <telhe008@uottawa.ca>
2022-03-31 15:15:53 -0400

    created a carousel and a double down arrow

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 $(function () {
-    $('.menu-toggle, .fa-times').on('click', function () {
-        $('nav').toggleClass('active');
-        $('.overlay').toggleClass('menu-open');
-    });
+  $(".menu-toggle, .fa-times").on("click", function () {
+    $("nav").toggleClass("active");
+    $(".overlay").toggleClass("menu-open");
+  });
 
-    $('.overlay').on('click', function () {
-        $('nav').removeClass('active');
-        $(this).removeClass('menu-open');
-    });
+  $(".overlay").on("click", function () {
+    $("nav").removeClass("active");
+    $(this).removeClass("menu-open");
+  });
 });
 
-document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
-    let currentDropdown
-    if (isDropdownButton) {
-        currentDropdown = e.target.closest("[data-dropdown]")
-        currentDropdown.classList.toggle("active")
-    }
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove("active")
-    })
-})
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
